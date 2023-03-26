de7d2f8074826a7988ea8af641cdb5b2b91625f7
pedroluca <pedrolucaofc@outlook.com>
2023-03-25 21:47:03 -0300

    feat: added non menu touch to close the nav sidebar

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -8,3 +8,12 @@ function toggleMenu(event) {
 
 buttonMobile.addEventListener('click', toggleMenu);
 buttonMobile.addEventListener('touchstart', toggleMenu);
+
+document.addEventListener('click', function(event) {
+  const menuMobile = document.getElementById('menu-mobile');
+  const isClickInsideMenu = menuMobile.contains(event.target);
+  const isClickInsideButton = buttonMobile.contains(event.target);
+  if (!isClickInsideMenu && !isClickInsideButton && menuMobile.classList.contains('active')) {
+    menuMobile.classList.remove('active');
+  }
+});
